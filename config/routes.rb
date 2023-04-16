0142e560ef654f3291e37b462f546aceb7533c0b
Tomas Valent <equivalent@eq8.eu>
2023-04-16 13:47:54 +0200

    standardrb lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :projects, only: [] do
-    resources :comments
+    resources :comments, only: [:create, :destroy, :update]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
