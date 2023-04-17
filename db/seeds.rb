e87225183328ee9cc308707fadf0d2c1e59251bb
Tomas Valent <equivalent@eq8.eu>
2023-04-17 11:35:50 +0200

    fix-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,12 @@ Project.find_or_create_by!(title: "Empty Project").tap do |project|
   project.save!
 end
 
-Project.find_or_create_by!(title: "Project with some conversation").tap do |project|
+Project.find_or_create_by!(title: "Project with conversation").tap do |project|
   project.description = "Our team is building a web application that will provide real-time data analytics and insights to property investors, helping them to make informed decisions about buying, selling, and managing real estate assets based on market trends and performance"
   project.save!
-  project.entries.create!(entryable: Event.create!(occasion: "project_closed"))
+  project.entries.create!(entryable: Event.create!(occasion: "project_closed"), creator: User.first)
 
-  project.entries.create!(entryable: Comment.create!(description: "lorem ipsum dolor sit amet"))
+  project.entries.create!(entryable: Comment.create!(description: "lorem ipsum dolor sit amet"), creator: User.last)
+  project.entries.create!(entryable: Event.create!(occasion: "project_opened"), creator: User.last)
+
 end
