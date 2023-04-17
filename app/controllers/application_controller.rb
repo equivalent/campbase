4929d4a7a953c54cd396fea587598a5d8981e51d
Tomas Valent <equivalent@eq8.eu>
2023-04-17 11:29:17 +0200

    require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def authenticate_user!
+    redirect_to sessions_url, notice: "Not authenticated" unless current_user
+  end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,5 +4,6 @@ class Entry < ApplicationRecord
 
   delegated_type :entryable, types: %w[Event Comment], dependent: :destroy
 
+  # todo: make the broadcasts secure like https://www.hotrails.dev/turbo-rails/turbo-streams-security
   broadcasts_to ->(entry) { "entries" }, inserts_by: :append
 end
